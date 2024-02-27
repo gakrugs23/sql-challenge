d040aba5e518512e53ae84f0a534848a0b8e6a84
Gabriella Kruger <gabbyolker@yahoo.com>
2024-02-26 19:20:28 -0600

    update to quereies

--- employee_db_queries.sql
+++ employee_db_queries.sql
@@ -23,8 +23,8 @@ ON dept_emp.emp_no = employees.emp_no
 JOIN departments
 ON dept_emp.dept_no = departments.dept_no;
 
--- 5. List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B"
-SELECT first_name, last_name
+-- 5. List first name, last name, and sex of each employee whose first name is Hercules and whose last name begins with the letter B.
+SELECT first_name, last_name, sex
 FROM employees
 WHERE first_name = 'Hercules'
 AND last_name LIKE 'B%';
